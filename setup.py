40b781919b47d81c4b2ee39d4960387ebbe513f8
martin <martin@defipay.biz>
2022-05-13 13:57:47 +0800

    版本号升级

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name="defipay",
-    version="0.10",
+    version="0.20",
     author="Defipay",
     author_email="support@defipay.biz",
     description="Defipay restful api",
